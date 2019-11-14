f07a4d89e47693bd6e931a418b6b610c50c33220
jbinde02 <jbinde02@rams.shepherd.edu>
2019-11-14 06:27:23 -0500

    Added Timer and TimerTask imports begin creating them

--- IntervalTimer.java
+++ IntervalTimer.java
@@ -1,5 +1,15 @@
-public class IntervalTimer{
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class IntervalTimer extends Timer{
+    Timer t;
+    TimerTask tt;
+    public IntervalTimer(){
+        t = new Timer();
+        t.schedule(tt, 1000);
+    }
     public static void main(String[] args) {
         System.out.println("Hello World");
+        IntervalTimer IT = new IntervalTimer();
     }
 }
